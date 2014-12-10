b80b09f43bb39667c26a74b583181b32763082e7
Worth Baker <wbaker@greenmtd.com>
2014-12-10 15:26:40 -0500

    Initial Commit

---
+++ SearchList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchList
+//
+//  Created by Worth Baker on 12/10/14.
+//  Copyright (c) 2014 Worth Baker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
